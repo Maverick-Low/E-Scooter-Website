926860c107d1aa303b8afcf8ac90f3d8f2aed2cf
Tamir Cohen <sc20tc@leeds.ac.uk>
2022-02-14 19:00:28 +0000

    Initial commit setting up flask file structure for app

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'a-very-secret-secret'
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
