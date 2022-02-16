2b47e87d4645c5e9213ebdc751475f92a4f942de
Adam Zbikowski <sc20aaz@leeds.ac.uk>
2022-02-16 17:08:50 +0000

    added dummy app routes for additional pages, created new branch for sprint 1

--- app/views.py
+++ app/views.py
@@ -5,3 +5,17 @@ from datetime import datetime
 @app.route("/")
 def test():
     return render_template("base.html")
+
+# TODO: replace return statements with actual templates
+
+@app.route("/register/")
+def register():
+    return 'registration page'
+
+@app.route("/login/")
+def signin():
+    return 'login page'
+
+@app.route("/home/")
+def home():
+    return 'home page'
