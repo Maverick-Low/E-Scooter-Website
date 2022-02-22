bb7510b76c384454f9284023b7fc550aa87a0136
Kevin Braszkiewicz <ed19km2b@leeds.ac.uk>
2022-02-22 15:05:11 +0000

    added model and tables code

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+from sqlalchemy import ForeignKey, false
 from app import db
 
 #Table for users
@@ -11,6 +13,25 @@ class User(db.Model):
 
 
 #Table for scooters
-#class Scooter(db.Model):
-#    __tablename__ = 'scooters'
-#    id =
+class Scooter(db.Model):
+    __tablename__ = 'scooters'
+    id = db.Column(db.Integer, primary_key=True)
+    in_use = db.Column(db.Boolean, default = False)
+    LocationID = db.Column(db.Integer, ForeignKey("locations.id"))
+
+
+#Table for locations
+class Location(db.Model):
+    __tablename__ = "locations"
+    id = db.Column(db.Integer, primary_key=True)
+
+
+#Table for orders/bookings
+class Booking(db.Model):
+    __tablename__ = "bookings"
+    id = db.Column(db.Integer, primary_key=True)
+    ScooterID = db.Column(db.Integer, ForeignKey("scooters.id"))
+    UserID = db.Column(db.Integer, ForeignKey("users.id"))
+    numHours = db.Column(db.Integer, nullable = False)
+    date = db.Column(db.DateTime, nullable = False)
+    price = db.Column(db.Integer, nullable = False)
