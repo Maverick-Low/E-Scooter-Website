f365322b01a92c4e13aa2b87cab930fe22da1b96
Maverick <sc20ml@leeds.ac.uk>
2022-02-23 19:00:57 +0000

    Payment slightly functional

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,17 @@
 from audioop import reverse
+from time import strftime
 from unicodedata import name
 from venv import CORE_VENV_DEPS
 from wsgiref.validate import validator
+# from app.test import luhns
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, DateField, IntegerField, SubmitField
-from wtforms.validators import EqualTo, DataRequired, ValidationError, Length, Email
+from sqlalchemy import Integer
+from wtforms import PasswordField, StringField, DateField, IntegerField, SubmitField, DateTimeField
+from wtforms.validators import EqualTo, DataRequired, ValidationError, Length, Email, NumberRange
 from app import models
-from datetime import date
+from datetime import date, datetime
 import re
+from flask import flash
 
 
 class Login(FlaskForm):
@@ -25,40 +29,17 @@ class Registration(FlaskForm):
 
     # function that validates email adress upon registration
     # todo: write unit tests for functions
-    def validateEmail(self, email):     
+    def validate_email(self, email):     
         user_object = models.User.query.filter_by(email=email.data).first()
         if user_object: #if user is already registered don't allow them to register again
             raise ValidationError('This email address is already registered!')
-
-        #regular expression for an email address with a single '.' after the '@':    
-        email_regex_1 = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-
-        #regular expression for an email address with two '.'s after the '@': 
-        email_regex_2 = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}+\.[A-Z|a-z]{2,}\b'
-        
-        if not( (re.fullmatch(email_regex_1,email)) or (re.fullmatch(email_regex_2)) ):
-                raise ValidationError('This email is not of a valid format!')
-
+            
 class Payment(FlaskForm):
-    card_number = IntegerField('Enter card number', validators=[DataRequired(), Length(max=20)])
-    cvv = IntegerField('Enter cvv', validators=[DataRequired(), Length(max=5)])
-    expiry_date = DateField('Enter expiry date', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-   
     #function that checks if the expiry date is in the future
-    def validateExpiryDate(self, expiry_date):
-        today = date.today()
-        if expiry_date < today:
-            raise ValidationError('Your card has expired')
+    # def validateCVV():
+    #     if not (len(self.cvv) == 3 or self.cvv == 4):
+    #         raise ValidationError('CVV is invalid')    
 
-
-    def validateCVV(self, cvv):
-        if len(cvv) > 4:
-            raise ValidationError('CVV is invalid')        
-    
-    
-    #function inspired by (Allwin Raju, 2020) available at:
-    # https://allwin-raju-12.medium.com/credit-card-number-validation-using-luhns-algorithm-in-python-c0ed2fac6234 
     def luhns(card_number):
         
         #check if card number is numeric
@@ -77,8 +58,37 @@ class Payment(FlaskForm):
         return checksum % 10
 
 
-    #todo: add code that determines type of credit card
-    def validateCardNumber(self, card_number):
-        if luhns(card_number) != 0:
+      #todo: add code that determines type of credit card
+    def validateCardNumber():
+        if luhns(self.card_number) != 0:
             raise ValidationError('Card number is invalid')
 
+
+    
+    name = StringField('Enter name', validators=[DataRequired(message = "Enter your name please"), Length(max=60)])
+    card_number = StringField('Enter card number', validators=[DataRequired(), Length(min=14, max=20)])
+    cvv = StringField('Enter cvv', validators=[DataRequired() , Length(min=3, max=4)])
+    expiry_date = DateTimeField('Enter expiry date (mm/YYYY)', validators=[DataRequired()], format='%m/%Y')
+    submit = SubmitField('Submit')
+
+    def validate_name(self, name):
+        excluded_chars = "*?!'^+%&/()=}][{$#£"
+        for char in self.name.data:
+            if char in excluded_chars:
+                raise ValidationError(
+                    f"Character {char} is not allowed in username.")
+   
+    # def validate_expiry_date(self, expiry_date_year):
+    #     today = datetime.now()  
+    #     today.strftime('%m/%Y')
+    #     if self.expiry_date < today:
+    #         raise ValidationError('Your card has expired')
+
+        
+    
+    
+    #function inspired by (Allwin Raju, 2020) available at:
+    # https://allwin-raju-12.medium.com/credit-card-number-validation-using-luhns-algorithm-in-python-c0ed2fac6234 
+    
+
+  
